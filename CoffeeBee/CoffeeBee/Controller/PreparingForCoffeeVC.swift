92e48527305b3e9ad94a579b0717644a1975a3e4
menescots <agatamenes@gmail.com>
2022-08-26 15:36:46 +0200

    UISlider ,calculate water coffee ratio

--- CoffeeBee/CoffeeBee/Controller/PreparingForCoffeeVC.swift
+++ CoffeeBee/CoffeeBee/Controller/PreparingForCoffeeVC.swift
@@ -20,19 +20,79 @@ class PreparingForCoffeeVC: UIViewController {
     @IBOutlet var coffeeValueLabel: UILabel!
     @IBOutlet var temperatureLabel: UILabel!
     
+    
+    @IBOutlet var fiveCups: UILabel!
+    @IBOutlet var fourCups: UILabel!
+    @IBOutlet var oneCup: UILabel!
+    @IBOutlet var twoCups: UILabel!
+    @IBOutlet var threeCups: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(named: "BackgroundColor")
         CoffeNameLabel.text = coffeeToPrepare
-       
     }
-    
     
     @IBAction func sliderValueDidChange(_ sender: UISlider) {
         let roundedStepValue = round(sender.value / step) * step
         sender.value = roundedStepValue
-        print("slider value; \(sender.value)")
-        waterValueLabel.text = String(Int(sender.value))
+        let intSliderValue = Int(sender.value)
+        guard let coffeeToPrepare = coffeeToPrepare else {
+            return
+        }
+        calculateWaterCoffeeRatio(selectedCoffee: coffeeToPrepare, water: intSliderValue)
+        highlightNumberOfCups(water: intSliderValue)
+        waterValueLabel.text = String(sender.value)
         
+    }
+    
+    private func calculateWaterCoffeeRatio(selectedCoffee: String, water: Int) {
+        switch selectedCoffee {
+        case "Turkish coffee":
+            coffeeValueLabel.text = String(water/12)
+        case "Moka pot":
+            coffeeValueLabel.text = String(water/12)
+        case "French press ":
+            coffeeValueLabel.text = String(water/15)
+        case "AeroPress":
+            coffeeValueLabel.text = String(water/16)
+        case "Syphon":
+            coffeeValueLabel.text = String(water/15)
+        case "Cold Brew":
+            coffeeValueLabel.text = String(water/8)
+        case "Chemex":
+            coffeeValueLabel.text = String(water/15)
+        case "Hario V60":
+            coffeeValueLabel.text = String(water/15)
+        case "Espresso":
+            coffeeValueLabel.text = String(water/2)
+        default:
+            return
+        }
+    }
+    
+    private func highlightNumberOfCups(water: Int){
+        
+        if water % 250 == 0{
+            switch water {
+            case 250:
+                oneCup.textColor = UIColor(named: "pinkColor")
+            case 500:
+                twoCups.textColor = UIColor(named: "pinkColor")
+            case 750:
+                threeCups.textColor = UIColor(named: "pinkColor")
+            case 1000:
+                fourCups.textColor = UIColor(named: "pinkColor")
+            case 1250:
+                fiveCups.textColor = UIColor(named: "pinkColor")
+            default:
+                break
+            }
+        } else {
+            oneCup.textColor = UIColor(named: "labelColor")
+                twoCups.textColor = UIColor(named: "labelColor")
+            threeCups.textColor = UIColor(named: "labelColor")
+                fourCups.textColor = UIColor(named: "labelColor")
+                fiveCups.textColor = UIColor(named: "labelColor")
+        }
     }
 }
